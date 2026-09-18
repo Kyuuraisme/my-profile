1e1733a1c04e16af03cd3966cdeaccd1fc2c7308
Ega-desu <egarisqi440@gmail.com>
2026-09-18 15:57:27 +0700

    menghapus file home.blade.php dan menambahkan route untuk profile

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/datadiri', [ProfileController::class, 'index']);
+
+Route::get('/', [ProfileController::class, 'index']);
 
